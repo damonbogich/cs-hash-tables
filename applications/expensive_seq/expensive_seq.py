de042bcc9bc52921f9fbbd314436886425ff47da
damonbogich <damonbogich@gmail.com>
2020-08-06 17:05:59 -0400

    expensive sequence solved

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,26 @@
-# Your code here
 
+#recursive function that will take forever to calculate if inputs are big enough 
+# want to store the results of (x,y,z) in cache so that the values will come up quickly
+
+#want to store the results of the function in a tuple => (x,y,z)
+
+#check in the function if the result of the function has been stored
+    #if it has not we want to run the function and store the answer
+    # if it has, we just want to return the answer from cache
+
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    my_key = (x, y, z)
+
+    if my_key in cache.keys():
+        return cache[my_key]
+    else:
+        if x <= 0:
+            cache[my_key] = y+z
+        else:
+            cache[my_key] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        return cache[my_key]
 
 
 
---
+++ applications/expensive_seq/test.py
@@ -0,0 +1,12 @@
+# exps(x, y, z) =
+#      if x <= 0: y + z
+#      if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+
+
+def exps(x, y, z):
+    if x <= 0:
+        return y+z
+    else:
+        return  exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+
+print(exps(1,2,3))
